dbd715f51b29ca19bd1015650644ab2ace5cdaea
Caoqizhi <2391580279@qq.com>
2023-12-20 14:32:28 +0800

    en: wagmi Init

---
+++ src/Providers.tsx
@@ -0,0 +1,16 @@
+
+
+import { WagmiConfig } from 'wagmi'
+import { wagmiConfig } from "./utils/wagmi";
+
+
+
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    
+    return (
+        <WagmiConfig config={wagmiConfig}>
+            {children}
+        </WagmiConfig>
+    )
+}
